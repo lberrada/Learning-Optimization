corrected ridge regression, implementing exterior derivative regression

main.py
@@ -2,6 +2,7 @@
 
 import pandas.io as pio
 import numpy.linalg as nlg
+import scipy.linalg as slg
 import statsmodels.api as sap
 import pandas as pd
 import numpy as np
@@ -24,8 +25,8 @@ def getData(filename):
     X = df[[key for key in predictors]]
     Y = df["quality"]
     
-    # add constant to X
-    X = sap.add_constant(X)
+#     # add constant to X
+#     X = sap.add_constant(X)
     return X, Y
 
 def ordinary_least_squares(X,Y, print_option = True):
@@ -34,14 +35,42 @@ def ordinary_least_squares(X,Y, print_option = True):
     :param X predictors data
     :param Y result
     :param print_option 
-    :return estimator (ols type)"""
+    :return: estimator"""
     
-    ols = sap.OLS(Y, X)
-    estimator = ols.fit()
+#     ols = sap.OLS(Y, X)
+#     estimator = ols.fit()
+#     if print_option:
+#         print(estimator.summary())
+    
+    XtX = np.matrix(np.dot(X.transpose(),X))
+    beta = np.dot(np.dot(nlg.inv(XtX),X.transpose()),Y)
     if print_option:
-        print(estimator.summary())
+        print beta.shape
+        print beta
     
-    return estimator
+    return beta
+
+def ordinary_least_squares_with_constant(X, Y, print_option = True):
+    """apply ordinary-least-squares regression on (X,Y) with an interception term
+    
+    :param X predictors data
+    :param Y result
+    :param print_option 
+    :return: estimator """
+    
+    # get data shape
+    n, p = X.shape
+    
+    # create data matrix by block
+    one_vec = np.ones((n,1))
+    big_X = np.concatenate([one_vec, X], axis = 1)
+    
+    # compute estimator
+    beta = ordinary_least_squares(big_X, Y, print_option)
+    
+    # return result
+    return beta
+    
 
 def ridge_regression(X, Y, mu, print_option = True):
     """apply ridge regression on (X,Y) with regularization parameter mu
@@ -50,38 +79,55 @@ def ridge_regression(X, Y, mu, print_option = True):
     :param Y result
     :param mu L2-regularization parameter
     :param print_option 
-    :return estimator (ols type)"""
+    :return: estimator"""
     
     # find n number of rows and p number of cols
     n, p = X.shape
     
     # create numpy arrays for block matrices (X)
     X_matrix = X.as_matrix()
-    id = np.identity(n)
-    mu_matrix = mu * np.ones((n,p))
-    zero_matrix = np.zeros((n,n))
+    one_vec = np.ones((n,1))
+    mu_matrix = mu * np.identity(p)
+    zero_vec = np.zeros((p,1))
     
     # concatenate in upper and lower block matrices (X)
-    upper_block = np.concatenate([id, X_matrix], axis = 1)
-    lower_block = np.concatenate([zero_matrix, mu_matrix], axis = 1)
+    upper_block = np.concatenate([one_vec, X_matrix], axis = 1)
+    lower_block = np.concatenate([zero_vec, mu_matrix], axis = 1)
     
     # concatenate in big matrix (X)
     big_X = np.concatenate([upper_block, lower_block], axis =0)
-    
+        
     # create numpy arrays for block matrices (Y)
     Y_vec = Y.as_matrix()
-    zero_vec = np.zeros(n)
     
     # concatenate in big vector
+    zero_vec = np.zeros(p)
     big_Y = np.concatenate([Y_vec, zero_vec])
-
+    
     # apply ordinary least squares on big matrices
     estimator = ordinary_least_squares(big_X, big_Y)
     
+    # retru result
     return estimator
+
+def exterior_derivative_estimation(X, Y, mu, d, print_option = True):
+    """apply exterior derivatice regression on (X,Y) with regularization parameter mu and dimension parameter d
+    
+    :param X predictors data
+    :param Y result
+    :param mu L2-regularization parameter
+    :param d number of dimensions kept from SVD
+    :param print_option 
+    :return: estimator"""
+    
+    # perform singular value decomposition
+    U, S, V = slg.svd(X)
+    
+    
+    
     
 
 X, Y = getData(filename)    
-# ordinary_least_squares(X, Y)
-ridge_regression(X, Y, 1)
+ordinary_least_squares_with_constant(X, Y)
+ridge_regression(X, Y, 1.5)
 